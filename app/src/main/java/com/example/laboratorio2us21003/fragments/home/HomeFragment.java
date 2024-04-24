dd7020539624ad7ce00c6fa5529588444d29ccb4
Alex Ulloa <US21003@ues.edu.sv>
2024-04-24 01:00:59 -0600

    Navegación entre las vistas de acciones

--- app/src/main/java/com/example/laboratorio2us21003/fragments/home/HomeFragment.java
+++ app/src/main/java/com/example/laboratorio2us21003/fragments/home/HomeFragment.java
@@ -1,14 +1,21 @@
 package com.example.laboratorio2us21003.fragments.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.laboratorio2us21003.R;
+import com.example.laboratorio2us21003.fragments.categories.CategoryFragment;
+import com.example.laboratorio2us21003.fragments.reports.ReportsFragment;
+import com.example.laboratorio2us21003.fragments.users.ProfileFragment;
+import com.example.laboratorio2us21003.fragments.vehicles.VehiclesFragment;
+import com.example.laboratorio2us21003.fragments.maintenances.MaintenanceFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
@@ -26,8 +33,9 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    public Fragment vehiclesFragment, mantainanceFragment, categoryFragment, reportsFragment;
+    public Fragment vehiclesFragment, mantainanceFragment, categoryFragment, reportsFragment, profileFragment;
     BottomNavigationView bottomNavigationViewOptions;
+    FragmentContainerView fragmentContainerView2;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -60,10 +68,43 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        vehiclesFragment = new VehiclesFragment();
+        mantainanceFragment = new MaintenanceFragment();
+        categoryFragment = new CategoryFragment();
+        reportsFragment = new ReportsFragment();
+        profileFragment = new ProfileFragment();
+        fragmentContainerView2 = root.findViewById(R.id.fragmentContainerView2);
+        bottomNavigationViewOptions = root.findViewById(R.id.bottomNavigationViewOptions);
+
+        bottomNavigationViewOptions.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.myVehicles:
+                    getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, vehiclesFragment).commit();
+                    break;
+                case R.id.mantainence:
+                    getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, mantainanceFragment).commit();
+                    break;
+                case R.id.categories:
+                    getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, categoryFragment).commit();
+                    break;
+                case R.id.reports:
+                    getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, reportsFragment).commit();
+                    break;
+                case R.id.my_profile:
+                    getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, profileFragment).commit();
+                    break;
+
+            }
+            return true;
+        });
+
+        return root;
     }
 }
